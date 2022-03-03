18b09d239db8a5bb8571576bfd7099048b534078
leda <Dat.Le@iconplc.com>
2022-03-03 17:42:45 +0700

    done propperty binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentToViewComponent } from './components/component-to-view/component-to-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentToViewComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/component-to-view/component-to-view.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'component-to-view',
+  templateUrl: './component-to-view.component.html',
+  styleUrls: ['./component-to-view.component.css']
+})
+export class ComponentToViewComponent implements OnInit {
+
+  public name: string = 'khoa';
+  public age: number = 34;
+  public isMarried: boolean = true;
+  public imgWidth : number = 300;
+  public isDisable: boolean = true;
+
+  public user : object = {
+    name: 'Dever',
+    age: 23,
+    isMarried:false
+  }
+
+
+  public imgLink : string = 'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRxXZmsthm2MMU3oXsQLr5Nx68Cd-rngbDJJZZ-djOZdFnnToFBEIIoyj3hqDCxfdfVoG2uuaA9a4c84nKU9Ks';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // DOES NOT WORK
+  // showInfo(){
+  //   return 'name:${this.user['name']}';
+  // }
+}
